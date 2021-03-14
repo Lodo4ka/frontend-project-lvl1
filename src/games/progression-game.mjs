7b0b5bb3ef186f6d0e80f4c9acb91e92f5b6213e
maxim.novikov <maxim.novikov@onlinesup.com>
2021-03-14 18:03:20 +0300

    remove function calculateEmptyOfProgression from progression game

--- src/games/progression-game.mjs
+++ src/games/progression-game.mjs
@@ -8,17 +8,6 @@ const removeElemProgression = (progression, removedIndex) => progression
 const generateProgression = (startProg, numberProg, randomLength) => Array
   .from({ length: randomLength }).map((_, index) => startProg + (index * numberProg));
 
-const calculateEmptyOfProgression = (progression, numberProg) => progression
-  .reduce((acc, elem, index) => {
-    if (elem === '..') {
-      if (progression[index - 1]) {
-        return acc + parseInt(progression[index - 1], 10) + parseInt(numberProg, 10);
-      }
-      return acc + parseInt(progression[index + 1], 10) - parseInt(numberProg, 10);
-    }
-    return acc + 0;
-  }, 0);
-
 const generateRoundGame = () => {
   const startProg = generateRandomNumber(0, 99);
   const numberProg = generateRandomNumber(0, 10);
@@ -26,7 +15,7 @@ const generateRoundGame = () => {
   const removedIndex = generateRandomNumber(0, randomLength);
   const progression = generateProgression(startProg, numberProg, randomLength);
   const progressionWithEmpty = removeElemProgression(progression, removedIndex);
-  const rightAnswer = String(calculateEmptyOfProgression(progression, numberProg));
+  const rightAnswer = String(progression[removedIndex]);
   const expression = progressionWithEmpty.join(' ');
   return {
     expression,
